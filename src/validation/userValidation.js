4e0a826f2dabe6e37b61ae8e2e9f5b0591c16d81
alexdev93 <alemayehu.dev@gmail.com>
2024-08-05 08:35:22 -1200

    add user and book schema

--- src/validation/userValidation.js
+++ src/validation/userValidation.js
@@ -13,7 +13,29 @@ const loginSchema = z.object({
   password: z.string().min(3, "Password must be at least 6 characters long"),
 });
 
+const bookSchema = z.object({
+  title: z.string().min(1, "Title is required"),
+  author: z.string().min(1, "Author is required"),
+  category: z.string().min(1, "Category is required"),
+  isApproved: z.boolean().optional(),
+  ownerId: z
+    .number()
+    .int()
+    .min(1, "Owner ID is required and should be a positive integer"),
+});
+
+const userSchema = z.object({
+  username: z.string().min(1, "Username is required"),
+  email: z.string().email("Invalid email format").optional(), // Optional, as it can be null
+  password: z.string().min(8, "Password must be at least 8 characters long"),
+  role: z.enum(["admin", "owner", "renter"], "Invalid role"),
+  status: z.enum(["active", "disabled"], "Invalid status"),
+  isApproved: z.boolean(),
+});
+
 module.exports = {
   registerSchema,
   loginSchema,
+  bookSchema,
+  userSchema
 };

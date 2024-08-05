edd06103267bfe8519f6c3e0b0c8f61591cb6600
alexdev93 <alemayehu.dev@gmail.com>
2024-08-04 17:54:36 -1200

    fix issues

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('./../controllers/userController');
+const authenticateToken = require('./../middleware/jwtMiddleware')
 const validate = require('../middleware/validationMiddleware');
 const { registerSchema, loginSchema } = require('../validation/userValidation');
 
 router.post('/register', validate(registerSchema), userController.register);
 router.post('/login', validate(loginSchema), userController.login);
-router.get('/profile', userController.getUserProfile);
+router.get('/profile', authenticateToken, userController.getUserProfile);
 
 module.exports = router;
--- src/validation/userValidation.js
+++ src/validation/userValidation.js
@@ -4,7 +4,7 @@ const registerSchema = z.object({
     username: z.string().min(3, 'Username must be at least 3 characters long'),
     password: z.string().min(6, 'Password must be at least 6 characters long'),
     location: z.string().min(2, 'location must have above two charachters'),
-    role: z.enum(['user', 'admin']),
+    role: z.enum(['user', 'admin', 'owner']),
 });
 
 const loginSchema = z.object({
